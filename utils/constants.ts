da85ea055f5f2c5ceca61d157bf64e118a740188
Mikalai Biadrytski <nickbiadrytski@gmail.com>
2022-12-05 14:46:22 +0300

    Updated HOME_PAGE_TITLE value

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,2 +1,2 @@
 export const baseUrl = 'https://mostly.ai/';
-export const HOME_PAGE_TITLE = 'MOSTLY AI: The Synthetic Data Company, Knowledge Hub and Generator - MOSTLY AI';
+export const HOME_PAGE_TITLE = 'MOSTLY AI: The Synthetic Data Generator and Knowledge Hub - MOSTLY AI';
